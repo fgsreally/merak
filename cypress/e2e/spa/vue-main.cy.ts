8707f03fa460e6a8f43d70af5ebdc6e09095b54e
fgsreally <1325041831@qq.com>
2023-03-30 15:59:06 +0800

    ci:add cypress cloud record

--- cypress/e2e/spa/vue-main.cy.ts
+++ cypress/e2e/spa/vue-main.cy.ts
@@ -4,7 +4,6 @@ import { checkModalColor, isSymbolExist } from '../utils'
 
 describe('main vue app [dev mode]', () => {
   beforeEach(() => {
-    cy.wait(1000)
     cy.visit(`${MAINAPP_URL.VUE}about`)
     cy.wait(1000)
   })
--- cypress/e2e/ssr/vue-ssr.cy.ts
+++ cypress/e2e/ssr/vue-ssr.cy.ts
@@ -4,8 +4,8 @@ import { getShadowSelector } from '../utils'
 
 describe('vue ssr [dev mode]', () => {
   beforeEach(() => {
-    cy.wait(1000)
     cy.visit(`${MAINAPP_URL.VUE_SSR}`)
+    cy.wait(1000)
   })
   it('should mount template on body', () => {
     cy.get('[data-merak-ssr="vite_vue"]').should('exist')

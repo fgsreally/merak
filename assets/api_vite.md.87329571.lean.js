8e2efdcef71bc08419ede5cf31d776c492144dee
fgsreally <fgsreally@users.noreply.github.com>
2023-07-19 08:24:13 +0000

    release V1 (#21)
    * feature(core):add windowVar/showEvent/UnmountEvent
    
    * feature(core):suport preload , refactor errorHandler
    
    * bk:replace globals with nativeVars and customVars
    
    * test:fix test for injectGlobal
    
    * test:fix test for injectGlobal
    
    * feature(core):add .namespace and .jump
    
    * feature:support debug/route/attribute/preload
    
    * e2e:pass test
    
    * test:pass analyse test
    
    * fix(compile):inject can handle customvar correctly in prod
    
    * types:add Props and NameSpace to improve type declaration
    
    * revert(core):remove debug
    
    * types(core):improve configOrUrl types
    
    * fix:turn merak.url to origin
    
    * fix(vue):add source option to merakimport
    
    * style:lint
    
    * fix(postcss):only handle :root(not include :host)  selector
    
    * wip:merge conflict
    
    * wip:merge conflict
    
    * fix(core):cacheFlag will sync keep-alive value
    
    * fix(vue):all components should not inheritAttrs
    
    * fix(core): only decode/encode merak app query
    
    * style(core): format warning output
    
    * release:rc.14
    
    * fix(webpack): fix types
    
    * e2e: fix e2e test
    
    * fix: fix type
    
    * chore: add suppressImplicitAnyIndexErrors to tsconfig
    
    * test: add local-pkg dep
    
    * revert: remove local-pkg
    
    * dependence: update dev dep
    
    * revert(compile): types error between local and ci
    
    * dependence: update vitest
    
    * e2e: add timeout to vue-main
    
    * e2e: remove hidden test in vue-main for now
    
    * docs: improve
    
    * fix(core): url doesn't have be an origin url
    
    * ready to realese v1~~

---
+++ assets/api_vite.md.87329571.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as l}from"./app.59ef58dc.js";const A=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"api/vite.md","lastUpdated":1689754953000}'),p={name:"api/vite.md"},o=l("",1),e=[o];function t(c,r,y,i,D,C){return n(),a("div",null,e)}const _=s(p,[["render",t]]);export{A as __pageData,_ as default};

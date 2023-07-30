86c8141a22840bf89dcef73e5e53251c744d8445
fgsreally <fgsreally@users.noreply.github.com>
2023-07-30 11:33:53 +0000

    Version Packages (#30)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ assets/api_vite.md.39ab4484.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as l}from"./app.b2a592f6.js";const A=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"api/vite.md","lastUpdated":1690716739000}'),p={name:"api/vite.md"},o=l("",1),e=[o];function t(c,r,y,i,D,C){return n(),a("div",null,e)}const _=s(p,[["render",t]]);export{A as __pageData,_ as default};

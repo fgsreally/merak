abb422533ce4cfc46010ee6c3d2ecdf5bdb41dd4
fgsreally <fgsreally@users.noreply.github.com>
2023-09-14 09:03:01 +0000

    release: v2.0.0-alpha!

--- assets/guide_idea_compare.md.52af2832.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c,a as d}from"./app.b2a592f6.js";const k=JSON.parse('{"title":"比较","description":"","frontmatter":{},"headers":[],"relativePath":"guide/idea/compare.md","lastUpdated":1694440648000}'),t={name:"guide/idea/compare.md"},r=d("",17),a=[r];function l(s,p,i,n,u,_){return o(),c("div",null,a)}const b=e(t,[["render",l]]);export{k as __pageData,b as default};

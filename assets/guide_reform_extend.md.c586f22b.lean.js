cfb453ad4ab5cf3d629011249a26724c15d4ef20
fgsreally <fgsreally@users.noreply.github.com>
2023-11-29 15:45:48 +0000

    Version Packages (#75)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ assets/guide_reform_extend.md.c586f22b.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as l}from"./app.ca02e63f.js";const A=JSON.parse('{"title":"继承","description":"","frontmatter":{},"headers":[],"relativePath":"guide/reform/extend.md","lastUpdated":1701272685000}'),p={name:"guide/reform/extend.md"},e=l("",5),o=[e];function t(c,r,y,i,D,F){return n(),a("div",null,o)}const d=s(p,[["render",t]]);export{A as __pageData,d as default};

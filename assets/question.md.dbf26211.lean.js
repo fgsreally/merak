10c35d7a1d7fa838c13f4cd87f5d82e679e6ad2d
fgsreally <fgsreally@users.noreply.github.com>
2024-02-11 15:33:51 +0000

    Dev (#87)
    * feature(vite): auto cors
    
    * docs: finish docs

---
+++ assets/question.md.dbf26211.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.b57a1379.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"question.md","filePath":"question.md","lastUpdated":1707665544000}'),o={name:"question.md"};function s(n,r,c,i,d,p){return t(),a("div")}const f=e(o,[["render",s]]);export{m as __pageData,f as default};

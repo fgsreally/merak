cb6af5ca43a261098d890049225bdd3e9cde4f1b
fgsreally <fgsreally@users.noreply.github.com>
2024-06-28 14:19:45 +0000

    Dev (#94)
    * feature(compile): Isolate globally declared variables and functions
    
    changeset md can't merge correctly
    
    * fix(core): refact proxy parameters, use merak instance instead
    
    * fix(core): fix proxy listener
    
    * ci: ignore ts-check in example
    
    * test: update snapshot

---
+++ assets/api_vue.md.73b41bb7.js
@@ -0,0 +1,91 @@
+import{_ as s,o as n,c as a,Q as p}from"./chunks/framework.31d25865.js";const d=JSON.parse('{"title":"vue","description":"","frontmatter":{},"headers":[],"relativePath":"api/vue.md","filePath":"api/vue.md","lastUpdated":1719584296000}'),l={name:"api/vue.md"},o=p(`<h1 id="vue" tabindex="-1">vue <a class="header-anchor" href="#vue" aria-label="Permalink to &quot;vue&quot;">​</a></h1><h2 id="merakapp-的配置" tabindex="-1">MerakApp 的配置 <a class="header-anchor" href="#merakapp-的配置" aria-label="Permalink to &quot;MerakApp 的配置&quot;">​</a></h2><p>事件与生命周期等同</p><div class="language-ts vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#F97583;">interface</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">MerakAppProps</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 即data-merak-id，必填</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">name</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">String</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 对应地址，必填</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">url</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">String</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 传给子应用的数据</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">props</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">PropType</span><span style="color:#E1E4E8;">&lt;</span><span style="color:#79B8FF;">any</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 使用的沙箱</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">proxy</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">PropType</span><span style="color:#E1E4E8;">&lt;</span><span style="color:#B392F0;">ProxyGlobals</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// iframeId</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">iframe</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">String</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 快速切换页面时，当卸载-&gt;挂载时间小于这个数，不会触发子应用钩子.timeout为0时，则一定会触发</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">timeout</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">Number</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">loaderOptions</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#6A737D;">// 传递给loader的值</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">PropType</span><span style="color:#E1E4E8;">&lt;</span><span style="color:#79B8FF;">any</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">loader</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">PropType</span><span style="color:#E1E4E8;">&lt;</span><span style="color:#B392F0;">Loader</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 初始跳转的路由</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">route</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">String</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 是否缓存样式，默认是</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">inlineStyle</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">Boolean</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#6A737D;">// 是否使用ssr</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#FFAB70;">ssr</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">type</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">Boolean</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#FFAB70;">default</span><span style="color:#F97583;">:</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">false</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#D73A49;">interface</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">MerakAppProps</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 即data-merak-id，必填</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">name</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">String</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 对应地址，必填</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">url</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">String</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 传给子应用的数据</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">props</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">PropType</span><span style="color:#24292E;">&lt;</span><span style="color:#005CC5;">any</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 使用的沙箱</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">proxy</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">PropType</span><span style="color:#24292E;">&lt;</span><span style="color:#6F42C1;">ProxyGlobals</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// iframeId</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">iframe</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">String</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 快速切换页面时，当卸载-&gt;挂载时间小于这个数，不会触发子应用钩子.timeout为0时，则一定会触发</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">timeout</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">Number</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">loaderOptions</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#6A737D;">// 传递给loader的值</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">PropType</span><span style="color:#24292E;">&lt;</span><span style="color:#005CC5;">any</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">loader</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">PropType</span><span style="color:#24292E;">&lt;</span><span style="color:#6F42C1;">Loader</span><span style="color:#24292E;">&gt;</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 初始跳转的路由</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">route</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">String</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 是否缓存样式，默认是</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">inlineStyle</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">Boolean</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6A737D;">// 是否使用ssr</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#E36209;">ssr</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">type</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#6F42C1;">Boolean</span></span>
+<span class="line"><span style="color:#24292E;">    </span><span style="color:#E36209;">default</span><span style="color:#D73A49;">:</span><span style="color:#24292E;"> </span><span style="color:#005CC5;">false</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div><h2 id="merakimport-merakscope" tabindex="-1">MerakImport/MerakScope <a class="header-anchor" href="#merakimport-merakscope" aria-label="Permalink to &quot;MerakImport/MerakScope&quot;">​</a></h2><p><a href="https://github.com/fgsreally/merak/blob/main/packages/vue/src/block.ts" target="_blank" rel="noreferrer">详见</a></p>`,6),e=[o];function c(t,r,y,E,i,F){return n(),a("div",null,e)}const B=s(l,[["render",c]]);export{d as __pageData,B as default};
